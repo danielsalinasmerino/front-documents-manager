78d36e59eaee0d1995ccfc3c22b93ef89af34495
danielsalinasmerino <danielsalinas305@gmail.com>
2021-03-15 22:57:55 +0100

    feat: Finished Modal, starting development to save content

--- src/components/section-modal-component/SectionModalComponent.js
+++ src/components/section-modal-component/SectionModalComponent.js
@@ -1,29 +1,54 @@
 import './SectionModalComponent.scss';
 import React, { useState } from 'react';
 
+// Project Components imports
+import StyledButtonComponent from '../styled-button-component/StyledButtonComponent';
+
 //Import images 
 import closeModal from '../../assets/images/close.png'; 
 
 function SectionModalComponent(props) {
 
-    const [title, setTitle] = useState("");
-    const [content, setContent] = useState("");
+    const [titleSection, setTitleSection] = useState("");
+    const [errorTitle, setErrorTitle ] = useState(false);
+
+    const [contentSection, setContentSection] = useState("");
+    const [titleDocument, setTitleDocument] = useState("");
+
+    function saveSection(){
+        
+        titleSection.trim();
+
+        if(titleSection.length === 0){
+            setErrorTitle(true);
+            var errorTitleDirty = true;
+        }
+
+        if(!errorTitleDirty){
+            props.closeModal();
+        }
+    }
+
+    function onChangeTitleSection(e){
+        setTitleSection(e.target.value);
+        setErrorTitle(false);
+    }
 
     return (
         <div className="sectionModalWrapper">
             <div className="modalHeader">
-                <p className="titleHeader">{props.title}</p>
+                <p className="titleHeader">Nueva sección</p>
                 <img className="closeModal" src={closeModal} alt="Close Modal" onClick={props.closeModal}/>
             </div>
             <div className="inputWrapper">
                 <p className="inputTitle">Título</p>
                 <input
-                    className="ownInput"
+                    className={errorTitle ? "ownInput error" : "ownInput"}
                     placeholder="Escriba el título de la sección (obligatorio)"
                     type="text"
-                    id="title"
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}/>
+                    id="titleSection"
+                    value={titleSection}
+                    onChange={(e) => onChangeTitleSection(e)}/>
             </div>
             <div className="inputWrapper">
                 <p className="inputTitle">Contenido</p>
@@ -31,19 +56,31 @@ function SectionModalComponent(props) {
                     className="ownTextarea"
                     placeholder="Escriba el contenido de la sección (opcional)"
                     type="text"
-                    id="content"
-                    value={content}
-                    onChange={(e) => setContent(e.target.value)}/>
+                    id="contentSection"
+                    value={contentSection}
+                    onChange={(e) => setContentSection(e.target.value)}/>
             </div>
             <div className="inputWrapper">
                 <p className="inputTitle">Documentos</p>
-                {/* <textarea
-                    className="ownTextarea"
-                    placeholder="Escriba el contenido de la sección (opcional)"
+                <input 
+                    className="ownInput"
+                    type="file" 
+                    id="file" 
+                    name="myfile"/>
+                <input
+                    className="ownInput"
+                    placeholder="Escriba el título de la sección (obligatorio)"
                     type="text"
-                    id="content"
-                    value={content}
-                    onChange={(e) => setContent(e.target.value)}/> */}
+                    id="titleDocument"
+                    value={titleDocument}
+                    onChange={(e) => setTitleDocument(e.target.value)}/>
+
+            </div>
+            <div className="bottomWrapper">
+                <StyledButtonComponent
+                    buttonText={'Guardar'}
+                    clickButton={saveSection}>
+                </StyledButtonComponent>
             </div>
         </div>
     );
