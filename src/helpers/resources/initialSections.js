f0eb15a3bcc95ba8b2342667c1b723c09a718dc0
danielsalinasmerino <danielsalinas305@gmail.com>
2021-03-19 12:48:49 +0100

    feat: Improved Dates on dummy sections

--- src/helpers/resources/initialSections.js
+++ src/helpers/resources/initialSections.js
@@ -10,8 +10,8 @@ export const initialSections = [
       makeId(),
       "Solicitud de actualización de incidencias en la Gestión Horaria",
       "Todas las solicitudes de actualización de incidencias en la Gestión Horaria deben estar firmadas por el interesado y validadas por el Responsable de Unidad/ Sección o Subdirector correspondiente",
-      new Date(),
-      new Date(),
+      new Date(2021, 2, 2, 0, 0, 0, 0),
+      new Date(2021, 2, 2, 0, 0, 0, 0),
       1, 
       null, // To do (parentID)
       null  // To do (portalID)
@@ -21,8 +21,8 @@ export const initialSections = [
       makeId(),
       "Modificación datos",
       "Este trámite debe realizarse a través de Politécnica Virtual",
-      new Date(),
-      new Date(),
+      new Date(2021, 2, 3, 0, 0, 0, 0),
+      new Date(2021, 2, 3, 0, 0, 0, 0),
       2, 
       null, // To do (parentID)
       null  // To do (portalID)
@@ -32,8 +32,8 @@ export const initialSections = [
       makeId(),
       "Solicitud genérica",
       "Esta solicitud es genérica y sirve para distintas ocasiones. Es importante poner en el pie del documento a quién va dirigida la solicitud. En el formato PDF vienen definidas las opciones más comunes.",
-      new Date(),
-      new Date(),
+      new Date(2021, 2, 5, 0, 0, 0, 0),
+      new Date(2021, 2, 5, 0, 0, 0, 0),
       3, 
       null, // To do (parentID)
       null  // To do (portalID)
@@ -43,8 +43,8 @@ export const initialSections = [
       makeId(),
       "Solicitud de Compatibilidad",
       "Es necesario presentar siempre cuatro documentos: 1. Solicitud de compatibilidad con una segunda actividad, ya sea en el sector privado o en el público (son impresos diferentes). 2. Certificado de horario en la Escuela. 3. Certificado del horario a realizar en la segunda empresa. 4. Una vida laboral actualizada. Impresos",
-      new Date(),
-      new Date(),
+      new Date(2021, 2, 8, 0, 0, 0, 0),
+      new Date(2021, 2, 8, 0, 0, 0, 0),
       4, 
       null, // To do (parentID)
       null  // To do (portalID)
@@ -54,8 +54,8 @@ export const initialSections = [
       makeId(),
       "Solicitud contratación temporal",
       "",
-      new Date(),
-      new Date(),
+      new Date(2021, 1, 2, 0, 0, 0, 0),
+      new Date(2021, 1, 2, 0, 0, 0, 0),
       5, 
       null, // To do (parentID)
       null  // To do (portalID)
@@ -65,8 +65,8 @@ export const initialSections = [
       makeId(),
       "Otros impresos UPM para PAS Laboral",
       "",
-      new Date(),
-      new Date(),
+      new Date(2021, 2, 10, 0, 0, 0, 0),
+      new Date(2021, 2, 10, 0, 0, 0, 0),
       6, 
       null, // To do (parentID)
       null  // To do (portalID)
@@ -76,8 +76,8 @@ export const initialSections = [
       makeId(),
       "Otros impresos UPM para PAS Funcionario",
       "",
-      new Date(),
-      new Date(),
+      new Date(2021, 1, 12, 0, 0, 0, 0),
+      new Date(2021, 1, 12, 0, 0, 0, 0),
       7, 
       null, // To do (parentID)
       null  // To do (portalID)
